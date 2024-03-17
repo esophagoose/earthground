4d648b0d0cb8f9e3172f9968d696a023f363f5cc
esophagoose <118142981+esophagoose@users.noreply.github.com>
2024-03-17 10:05:15 -0700

    Removed libraries from gitignore

--- earthground/library/integrated_circuits/interface_sensor/cypress_mbr3.py
+++ earthground/library/integrated_circuits/interface_sensor/cypress_mbr3.py
@@ -1,6 +1,6 @@
-import earthground.schematic as sch
 import earthground.components as cmp
 import earthground.library.protocols.serial as serial
+import earthground.schematic as sch
 from earthground.library.footprints import qfn
 
 
@@ -53,12 +53,12 @@ class CY8CMBR3116(cmp.Component):
             },
             self,
         )
-        self.i2c = serial.I2C(sda=self.pins.by_name("SDA"),
-                              scl=self.pins.by_name("SCL"))
-        self.footprint = qfn.Qfn(pin_count=24,
-                                 size=qfn.PackageSize.S4_0MMx4_0MM,
-                                 pitch=0.5,
-                                 ep=(2.65, 2.65))
+        self.i2c = serial.I2C(
+            sda=self.pins.by_name("SDA"), scl=self.pins.by_name("SCL")
+        )
+        self.footprint = qfn.Qfn(
+            pin_count=24, size=qfn.PackageSize.S4_0MMx4_0MM, pitch=0.5, ep=(2.65, 2.65)
+        )
 
     def get_sense_pin(self, index):
         name = f"CS{index}"
@@ -70,8 +70,7 @@ class CY8CMBR3116(cmp.Component):
 def generate_design(vdd: float, i2c_pullup="4.7k", i2c_series_res="330"):
     assert 1.8 <= vdd <= 5.5, "Invalid voltage range! 1.8V - 5.5V"
     total_pins = CY8CMBR3116.TOTAL_SENSE_PINS
-    ports = [f"CS{i}"
-             for i in range(total_pins)] + ["VDD", "GND", "I2C", "nRESET"]
+    ports = [f"CS{i}" for i in range(total_pins)] + ["VDD", "GND", "I2C", "nRESET"]
     design = sch.Design("Mbr3Design", "MBR", ports)
     mbr3 = design.add_component(CY8CMBR3116())
 
@@ -83,17 +82,13 @@ def generate_design(vdd: float, i2c_pullup="4.7k", i2c_series_res="330"):
     design.port.nreset = mbr3.pins.by_name("XRES")
 
     # Decoupling capacitors
-    design.add_decoupling_cap(mbr3.pins.by_name("CMOD"),
-                              cmp.Capacitor("2.2n", 10))
-    design.add_decoupling_cap(mbr3.pins.by_name("VDD"),
-                              cmp.Capacitor("1u", 10))
-    design.add_decoupling_cap(mbr3.pins.by_name("VDD"),
-                              cmp.Capacitor("0.1u", 10))
+    design.add_decoupling_cap(mbr3.pins.by_name("CMOD"), cmp.Capacitor("2.2n", 10))
+    design.add_decoupling_cap(mbr3.pins.by_name("VDD"), cmp.Capacitor("1u", 10))
+    design.add_decoupling_cap(mbr3.pins.by_name("VDD"), cmp.Capacitor("0.1u", 10))
     if vdd < 1.89:
         design.connect([mbr3.pins.by_name("VDD"), mbr3.pins.by_name("VCC")])
     else:
-        design.add_decoupling_cap(mbr3.pins.by_name("VCC"),
-                                  cmp.Capacitor("0.1u", 10))
+        design.add_decoupling_cap(mbr3.pins.by_name("VCC"), cmp.Capacitor("0.1u", 10))
 
     # I2C series resistors
     design.port.i2c = mbr3.i2c
@@ -106,10 +101,12 @@ def generate_design(vdd: float, i2c_pullup="4.7k", i2c_series_res="330"):
 
     # Pull-up resistors for I2C
     if i2c_pullup:
-        design.add_series_res(mbr3.pins.by_name("VDD"), i2c_pullup,
-                              mbr3.pins.by_name("SCL"))
-        design.add_series_res(mbr3.pins.by_name("VDD"), i2c_pullup,
-                              mbr3.pins.by_name("SDA"))
+        design.add_series_res(
+            mbr3.pins.by_name("VDD"), i2c_pullup, mbr3.pins.by_name("SCL")
+        )
+        design.add_series_res(
+            mbr3.pins.by_name("VDD"), i2c_pullup, mbr3.pins.by_name("SDA")
+        )
 
     # Add CapSense pins to ports
     for i in range(total_pins):
